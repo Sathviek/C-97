85202c2e3ed23fc87973ded270f898808ac5c034
Sathviek <71971661+Sathviek@users.noreply.github.com>
2022-02-01 18:58:34 +0530

    Add files via upload

--- P-97.py
+++ P-97.py
@@ -1,8 +1,15 @@
 heading="Number guessing game"
+chances=1
+number=6
 print(heading)
-EnterNumber="Guess a number between 1 and 9"
+
+SubHeading="Guess a number between 1 and 9"
+print(SubHeading)
 while chances < 5:
+    guess=input("Enter Number")
+    chances=chances+1
+    print(chances)
     if guess == number:
         print("Congratulations you won")
-        if not chances < 5:
-        print("You Lost")
+        #if not chances < 5:
+        #print("You Lost")
